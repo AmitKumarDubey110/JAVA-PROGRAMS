6068962444705e9a4d73114c60a1f4201d7df469
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-04-24 23:29:09 +0530

    update

---
+++ Javaif.java
@@ -0,0 +1,19 @@
+//To illustrate the if else statement.
+
+
+class Javaif
+{
+ public static void main(String args[])
+ {
+  int a=10;
+  int b=20;
+  if(a>b)
+  {
+   System.out.println("Value:"+a);
+  }
+  else
+  {
+  System.out.println("value:"+b);
+  }
+ }
+}

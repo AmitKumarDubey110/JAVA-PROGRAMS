199e0601dd89b4b8d6e0139f294d587a454375ac
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-04-27 23:25:22 +0530

    update

---
+++ ForDemo.java
@@ -0,0 +1,13 @@
+//To illustrate for statement to print the numbers 1 through 10 to standard output.
+
+
+class ForDemo
+{
+ public static void main(String args[])
+ {
+  for(int i=1;i<11;i++)
+  {
+   System.out.println("Count is:"+i);
+  }
+ }
+}

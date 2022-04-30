7c29e61de784b0b2c1206f95d2b51e40e6f8ec42
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-04-30 22:59:08 +0530

    update

---
+++ OneDArray.java
@@ -0,0 +1,10 @@
+public class OneDArray{
+	public static void main(String args[])
+	{
+	 int[] iArray=new int[]{1,2,3,4,5};
+
+	for(int i=0;i<iArray.length;i++){
+	 System.out.println(iArray[i]+",");
+	 }
+	}
+}

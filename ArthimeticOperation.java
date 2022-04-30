35d217d2c677dcde1e4194ed2f0fcda255b2e1de
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-04-30 22:57:46 +0530

    update

---
+++ ArthimeticOperation.java
@@ -0,0 +1,15 @@
+//Illustrating Arthimetic operation. To illustrate the comparison operators.
+
+public class ArthimeticOperation{
+	public static void main(String args[])
+	{
+	 int value1=5;
+	 int value2=5; 
+         System.out.println("Addtion of the values:"+(value1+value2));
+	 System.out.println("Substraction of the values:"+(value1-value2));
+	 System.out.println("Multiplication of the values:"+(value1*value2));
+	 System.out.println("Division of the values:"+(value1/value2));
+	 System.out.println("Modulus of the values:"+(value1%value2));
+	   
+	}
+}
